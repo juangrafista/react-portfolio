2bb376e9bc5f18f15d8fedd688016dee38b2318e
juangrafista <juangrafista@gmail.com>
2020-12-07 19:01:59 -0500

    9. React router intro

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import GlobalStyle from './components/GlobalStyle'
-
 import Nav from './components/Nav'
 import AboutUs from './pages/AboutUs'
+import OurWork from './pages/OurWork'
+import ContactUs from './pages/ContactUs'
+
+import GlobalStyle from './components/GlobalStyle'
+// Router
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path='/' exact>
+          <AboutUs />
+        </Route>
+        <Route path='/work'>
+          <OurWork />
+        </Route>
+        <Route path='/contact'>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   )
 }
